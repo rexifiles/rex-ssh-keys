74b3a9cfa965185892bce9c6a9a1ed0081a0c1d4
xarlos <xarlos@command1.net.xarlos.me>
2015-12-31 14:21:09 +0000

    Requires home argument

--- __module__.pm
+++ __module__.pm
@@ -5,15 +5,17 @@ use Rex::Ext::ParamLookup;
 user 'root';
 key_auth;
 
-# Usage: rex setup 
+# Usage: rex setup ( user => "root" home => "/root" sshkey => "ssh-rsa AAAAAAAAAAA ... user@place" )
 
 desc 'Add in the ssh keys for my user';
 task 'setup', sub { 
 
 	my $user     = param_lookup "user";
+	my $home     = param_lookup "home";
 	my $sshkey   = param_lookup "sshkey";
 	
 	account "$user",
+		home    => "$home";
 		ssh_key => "$sshkey";
 
 };
